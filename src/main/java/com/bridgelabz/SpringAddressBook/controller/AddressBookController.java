8ffac1b9d8c96f93fc36375db87fc0c0786425d2
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-03-04 15:57:33 +0530

    [Unaiz Siddiqui] - Rest Api

---
+++ src/main/java/com/bridgelabz/SpringAddressBook/controller/AddressBookController.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.SpringAddressBook.controller;
+
+
+
+
+
+import com.bridgelabz.SpringAddressBook.model.AddressBook;
+import com.bridgelabz.SpringAddressBook.service.AddressBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/addressbook")
+public class AddressBookController {
+
+    @Autowired
+    private AddressBookService service;
+
+    @PostMapping("/add")
+    public ResponseEntity<AddressBook> addEntry(@RequestBody AddressBook addressBook) {
+        return ResponseEntity.ok(service.saveEntry(addressBook));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<AddressBook>> getAllEntries() {
+        return ResponseEntity.ok(service.getAllEntries());
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<AddressBook> getEntryById(@PathVariable Long id) {
+        return service.getEntryById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<AddressBook> updateEntry(@PathVariable Long id, @RequestBody AddressBook newEntry) {
+        return ResponseEntity.ok(service.updateEntry(id, newEntry));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteEntry(@PathVariable Long id) {
+        service.deleteEntry(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/bridgelabz/SpringAddressBook/repository/AddressBookRepository.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.SpringAddressBook.repository;
+
+
+
+
+import com.bridgelabz.SpringAddressBook.model.AddressBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressBookRepository extends JpaRepository<AddressBook, Long> {
+}
