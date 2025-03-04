a2b240a88d2b65a0247e12b735189abaa80293ac
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-03-04 16:04:05 +0530

    [Unaiz Siddiqui] - Add DTO in the application

--- src/main/java/com/bridgelabz/SpringAddressBook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/SpringAddressBook/dto/AddressBookDTO.java
@@ -1,4 +1,15 @@
 package com.bridgelabz.SpringAddressBook.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressBookDTO {
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
 }
