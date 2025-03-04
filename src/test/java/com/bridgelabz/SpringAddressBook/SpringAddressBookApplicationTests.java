53e9e258306e228ceac96ad0659584b2f0d21658
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-03-04 15:51:38 +0530

    [Unaiz Siddiqui] - application Setup and sql connection

---
+++ src/test/java/com/bridgelabz/SpringAddressBook/SpringAddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.SpringAddressBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
